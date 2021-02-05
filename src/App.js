87862aafabc75400cc844ec96dd7d0139d7f2429
doniarobinson <doniamae@gmail.com>
2021-02-05 14:26:43 -0600

    Feb 6 interview pair

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [characters, setCharacters] = useState([]);
+  let [films, setFilms] = useState([]);
+
+  function handleClick() {
+    fetch('https://www.swapi.tech/api/people').then(res =>
+      res.json().then((result, error) => {
+        setCharacters(result.results);
+      })
+    );
+  }
+
+  function handleCharacterClick() {
+    fetch('https://www.swapi.tech/api/films/').then(res =>
+      res.json().then((result, error) => {
+        // do some parsing for the person that was clicked on
+        //setFilms();
+        // update films for correct person
+      })
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"/>
-       </header>
+    <div className='App'>
+      <button onClick={handleClick}>Get Characters</button>
+      <div id='charList'>
+        <ul>
+          {characters.map((character, i) => (
+            <li key={i}>
+              <button onClick={handleCharacterClick}>{character.name}</button>
+              <div className='films'></div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
